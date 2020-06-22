4f9180c99fcf35e1ebf65e18c83e114bbd80f638
Marc Duiker <marcduiker@gmail.com>
2020-06-22 23:33:15 +0200

    Added Terraform config

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "rg_name" {
+    description = "Name of the Resources Group"
+}
+
+variable "serviceplan_name" {
+    description = "Name of the App Service Plan"
+}
+
+
+variable "appservice_name" {
+    description = "Name of the App Service"
+}
+
+variable "location" {
+    description = "Name of the Azure region"
+}
