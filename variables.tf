3477846de90a70ab042be562536369af12b61220
Marc Duiker <marcduiker@gmail.com>
2020-06-23 00:51:56 +0200

    Added types to Terraform variables and added ENV vars for Azure

--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
 variable "rg_name" {
+    type = string
     description = "Name of the Resources Group"
 }
 
 variable "serviceplan_name" {
+    type = string
     description = "Name of the App Service Plan"
 }
 
-
 variable "appservice_name" {
+    type = string
     description = "Name of the App Service"
 }
 
 variable "location" {
+    type = string
     description = "Name of the Azure region"
 }
