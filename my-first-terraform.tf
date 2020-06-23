f1f1ed3f7fc6d433658a70554ff9aea80000a6f1
Marc Duiker <marcduiker@gmail.com>
2020-06-23 09:00:39 +0200

    Autoformatted

--- my-first-terraform.tf
+++ my-first-terraform.tf
@@ -4,17 +4,17 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-    name        = var.rg_name
-    location    = var.location
-    tags        = {
-        Environment = "Terraform on Azure"
-    } 
+  name     = var.rg_name
+  location = var.location
+  tags = {
+    Environment = "Terraform on Azure"
+  }
 }
 
 resource "azurerm_app_service_plan" "serviceplan" {
-    name                = var.serviceplan_name
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
+  name                = var.serviceplan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   sku {
     tier = "Standard"
@@ -23,18 +23,18 @@ resource "azurerm_app_service_plan" "serviceplan" {
 }
 
 resource "azurerm_app_service" "appservice" {
-  name                  = var.appservice_name
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  app_service_plan_id   = azurerm_app_service_plan.serviceplan.id
-  app_settings          = {
+  name                = var.appservice_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.serviceplan.id
+  app_settings = {
     "WEBSITE_NODE_DEFAULT_VERSION" = "10.15.2"
-    "ApiUrl" = ""
-    "ApiUrlShoppingCart" = ""
-    "MongoConnectionString" = ""
-    "SqlConnectionString" = ""
-    "productImagesUrl" = "	https://raw.githubusercontent.com/microsoft/TailwindTraders-Backend/master/Deploy/tailwindtraders-images/product-detail"
-    "Personalizer__ApiKey" = ""
-    "Personalizer__Endpoint" = ""
+    "ApiUrl"                       = ""
+    "ApiUrlShoppingCart"           = ""
+    "MongoConnectionString"        = ""
+    "SqlConnectionString"          = ""
+    "productImagesUrl"             = "	https://raw.githubusercontent.com/microsoft/TailwindTraders-Backend/master/Deploy/tailwindtraders-images/product-detail"
+    "Personalizer__ApiKey"         = ""
+    "Personalizer__Endpoint"       = ""
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "rg_name" {
-    type        = string
-    description = "Name of the Resources Group"
+  type        = string
+  description = "Name of the Resources Group"
 }
 
 variable "serviceplan_name" {
-    type        = string
-    description = "Name of the App Service Plan"
+  type        = string
+  description = "Name of the App Service Plan"
 }
 
 variable "appservice_name" {
-    type        = string
-    description = "Name of the App Service"
+  type        = string
+  description = "Name of the App Service"
 }
 
 variable "location" {
-    type = string
-    description = "Name of the Azure region"
+  type        = string
+  description = "Name of the Azure region"
 }
